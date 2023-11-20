b06437ffd95902f92e89088192ae384b9e5b7b3e
LuciferLovesMe <abhiemsmile@gmail.com>
2023-11-21 00:47:06 +0700

    feat/be-templating: fixing templating mvc

--- App/config/App.php
+++ App/config/App.php
@@ -8,47 +8,48 @@ class App{
     public function __construct()
     {
         $url = $this->parseUrl();
-        $controllerName = $url[0] == 'API' ? $url[1] : $url[0];
-        $methodName = $url[0] == 'API' ? $url[2] : $url[1];
+        $methodName = $url[3];
         
-        if($url[0] == 'API'){
+        if($url[1] == 'API'){
             // FOR API CONTROLLER
-            if(file_exists("../App/controllers/API/" . ucfirst($url[1]) . "Controller.php")){
-                $this->controller = ucfirst($url[1]) . 'Controller'; 
-                unset($url[1]);
+            if(file_exists("../App/controllers/API/" . ucfirst($url[2]) . "Controller.php")){
+                $this->controller = ucfirst($url[2]) . 'Controller'; 
+                unset($url[2]);
             }
 
             // GET Controller
             require_once '../App/controllers/API/' . $this->controller . '.php';
             $this->controller = new $this->controller;
 
-            if(isset($url[2])){
+            if(isset($url[3])){
                 if(method_exists($this->controller, $methodName)){
                     $this->method = $methodName;
-                    unset($url[2]);
+                    unset($url[3]);
                 }
             }
-        } else {
+        } else if($url[1] == 'admin') {
             // FOR NON API CONTROLLER
-            if(file_exists("../App/controllers/" . ucfirst($url[0]) . "Controller.php")){
-                $this->controller = ucfirst(ucfirst($url[0])) . 'Controller'; 
-                unset($url[0]);
+            if(file_exists($_SERVER['DOCUMENT_ROOT'] . "/App/controllers/" . $url[2] . "Controller.php")){
+                $this->controller = (($url[2])) . 'Controller'; 
+                unset($url[2]);
             }
 
             // GET Controller
-            require_once "../App/controllers/" . $this->controller . '.php';
+            require_once $_SERVER['DOCUMENT_ROOT'] . "/App/controllers/" . $this->controller . '.php';
             $this->controller = new $this->controller;
     
-            if(isset($url[1])){
+            if(isset($url[3])){
                 if(method_exists($this->controller, $methodName)){
                     $this->method = $methodName;
-                    unset($url[1]);
+                    unset($url[3]);
                 }
             }
         }
 
         // Parsing parameter
         if(!empty($url)){
+            unset($url[0]);
+            unset($url[1]);
             $this->params = array_values($url);
         } else{
             $this->params = [];
@@ -59,8 +60,8 @@ class App{
     }
 
     public function parseUrl(){
-        if( isset($_GET['url']) ){
-            $url = rtrim($_GET['url'], '/');
+        if( isset($_SERVER['REQUEST_URI']) ){
+            $url = rtrim($_SERVER['REQUEST_URI'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
             $url = explode('/', $url);
             return $url;
--- App/controllers/Controller.php
+++ App/controllers/Controller.php
@@ -3,7 +3,7 @@
 class Controller{
     private $token = 'KgncmLUc7qvicKI1OjaLYLkPi';
     public function view($view, $data = array()){
-        require_once '../App/views/' . $view . '.php';
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/views/' . $view . '.php';
     }
 
     public function getToken(){
