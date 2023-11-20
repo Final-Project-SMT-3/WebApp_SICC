cc77e39e7795152c08bb8b0972e67f000a2e6bcd
rayrayaray <rayasya.dziqi@gmail.com>
2023-11-20 08:23:06 +0700

    fix templating

--- views/user/components/head.php
+++ views/user/components/head.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/assets/landing_page/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/landing_page/css/styles.css">
+    <link rel="stylesheet" href="/public/assets/landing_page/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/public/assets/landing_page/css/styles.css">
 </head>
 
 <body>
--- views/user/components/scripts.php
+++ views/user/components/scripts.php
@@ -1,6 +1,6 @@
-<script src="/assets/landing_page/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="/assets/landing_page/js/script.js"></script>
-<script src="assets/landing_page/vendor/aos/aos.js"></script>
+<script src="/public/assets/landing_page/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/public/assets/landing_page/js/script.js"></script>
+<script src="/public/assets/landing_page/vendor/aos/aos.js"></script>
 </body>
 
 </html>
